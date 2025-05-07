d50c0de48d4b5d2cf4613abb40fa1b56aef045c4
Kilmer11 <kilmerandrade45@gmail.com>
2025-05-07 10:07:50 -0300

    Finalização config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,27 @@
+import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import prettierPlugin from 'eslint-plugin-prettier';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat();
 
 export default [
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,mjs}'],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
       globals: {
         ...globals.browser,
+      },
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
     plugins: {
